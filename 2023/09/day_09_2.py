52a3a1c6b10ea32b62f64d470b5c2ba0ae312f80
Abdelhalim Chekroun <ab.chekroun.ext@axione.fr>
2023-12-09 07:14:20 +0100

    09/12/2023

---
+++ 2023/09/day_09_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+def get_deltas(sequence: list[int]) -> list[int]:
+    return [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
+
+
+def extrapolate(sequence: list[int]) -> int:
+    if all(x == 0 for x in sequence):
+        return 0
+    return sequence[-1] + extrapolate(get_deltas(sequence))
+
+
+def parse(day_input: str) -> list[list[int]]:
+    return [
+        [int(n) for n in line.split(" ")]
+        for line in day_input.split("\n")
+    ]
+
+
+if __name__ == "__main__":
+    input_ = open("input").read().strip()
+    list_sequences = parse(input_)
+    res = [extrapolate(list(reversed(s))) for s in list_sequences]
+    print(sum(res))
