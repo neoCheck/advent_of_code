b5417b5d5d79a01a272d852ef6940a610146a5ad
Abdelhalim Chekroun <ab.chekroun.ext@axione.fr>
2024-12-08 16:41:02 +0100

    04/12/2015

---
+++ 2015/04/day_04.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import hashlib
+
+
+def main() -> None:
+    day_input = "bgvyzdsv"
+
+    i = 0
+    while True:
+        i += 1
+        res = hashlib.md5(f"{day_input}{i}".encode("utf-8")).hexdigest()
+        if res.startswith("00000"):
+            print(f"{i}: {res}")
+            break
+
+
+
+if __name__ == "__main__":
+    main()
